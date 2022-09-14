2f76a88fda3fc2f38627fe16a55219813987f72f
jaehyeonha <mini0950@knu.ac.kr>
2022-09-14 11:12:38 +0900

    local folder name corret

--- venvs/mysite.cmd
+++ venvs/mysite.cmd
@@ -1,3 +1,3 @@
 @echo off
-cd c:/Users/Ha Jaehyeon/Study_django/mysite
-"c:/Users/Ha Jaehyeon/Study_django/venvs/mysite/scripts/activate"
+cd c:/Users/Ha Jaehyeon/Study_Django/mysite
+"c:/Users/Ha Jaehyeon/Study_Django/venvs/mysite/scripts/activate"
