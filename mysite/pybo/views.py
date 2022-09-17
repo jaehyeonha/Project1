6716238b47e3841fa092fc54e798a8033b710052
jaehyeonha <mini0950@knu.ac.kr>
2022-09-17 17:27:34 +0900

    3-7 plus author

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,6 +4,7 @@ from .models import Question
 from .forms import QuestionForm, AnswerForm
 from django.utils import timezone 
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 def index (request):
     """
     pybo 목록 출력
@@ -27,6 +28,7 @@ def detail (request, question_id):
     context = {'question':question}
     return render(request,'pybo/question_detail.html',context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id): 
     """
     pybo 답변 등록 
@@ -36,6 +38,7 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -45,6 +48,7 @@ def answer_create(request, question_id):
     context = {'question': question, 'form': form}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def question_create(request): 
     """
     pybo 질문 등록 
@@ -53,6 +57,7 @@ def question_create(request):
         form = QuestionForm(request.POST) 
         if form.is_valid(): 
             question = form.save(commit=False) 
+            question.author=request.user
             question.create_date = timezone.now() 
             question.save() 
             return redirect('pybo:index') 
